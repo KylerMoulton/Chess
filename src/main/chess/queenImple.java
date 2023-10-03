e981fa67d9dfea941d42530b569db86151d7b386
Kyler Moulton <kylermoulton@gmail.com>
2023-10-03 15:49:56 -0600

    Queen Moves

---
+++ src/main/chess/queenImple.java
@@ -0,0 +1,25 @@
+package chess;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class queenImple extends pieceImple{
+    public queenImple(ChessGame.TeamColor teamColor) {
+        super(teamColor, PieceType.QUEEN);
+    }
+
+    @Override
+    public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        Set<ChessMove> possibleMoves = new HashSet<>();
+        possibleMoves.addAll(lineMoves(board,myPosition,1,0));
+        possibleMoves.addAll(lineMoves(board,myPosition,-1,0));
+        possibleMoves.addAll(lineMoves(board,myPosition,0,1));
+        possibleMoves.addAll(lineMoves(board,myPosition,0,-1));
+        possibleMoves.addAll(lineMoves(board,myPosition,1,1));
+        possibleMoves.addAll(lineMoves(board,myPosition,1,-1));
+        possibleMoves.addAll(lineMoves(board,myPosition,-1,1));
+        possibleMoves.addAll(lineMoves(board,myPosition,-1,-1));
+        return possibleMoves;
+    }
+}
