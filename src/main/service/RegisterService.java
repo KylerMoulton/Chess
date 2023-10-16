63210c60dfcbb35cf41430525bb4bde7501c1eb8
Kyler Moulton <kylermoulton@gmail.com>
2023-10-16 12:48:47 -0600

    Register Classes

---
+++ src/main/service/RegisterService.java
@@ -0,0 +1,10 @@
+package service;
+
+import request.RegisterRequest;
+import result.RegisterResult;
+
+public class RegisterService {
+    public RegisterResult register(RegisterRequest r) throws Exception{
+        return null;
+    }
+}
