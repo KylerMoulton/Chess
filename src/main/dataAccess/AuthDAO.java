63765b4e217fb43ff223df8de8931251832d80c1
Kyler Moulton <kylermoulton@gmail.com>
2023-10-16 12:41:27 -0600

    Auth Data Access Object

---
+++ src/main/dataAccess/AuthDAO.java
@@ -0,0 +1,21 @@
+package dataAccess;
+
+import model.AuthTokenModel;
+
+public class AuthDAO {
+    void createToken(AuthTokenModel token) {
+
+    }
+    AuthTokenModel getToken() {
+        return null;
+    }
+    void updateToken(String token) {
+
+    }
+    void updateUsername(String username) {
+
+    }
+    void deleteToken(AuthTokenModel token) {
+
+    }
+}
