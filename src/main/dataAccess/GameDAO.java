625d3505dfe97c5d98a91daa15e4216cdda80e30
Kyler Moulton <kylermoulton@gmail.com>
2023-10-16 12:45:53 -0600

    Game Model and Data Access

---
+++ src/main/dataAccess/GameDAO.java
@@ -0,0 +1,28 @@
+package dataAccess;
+
+import chess.ChessGame;
+import model.GameModel;
+
+public class GameDAO {
+    void insertGame(GameModel game) throws DataAccessException{
+
+    }
+    void getGame(GameModel game) throws DataAccessException{
+
+    }
+    void getAllGames() throws DataAccessException{
+
+    }
+    void claimSpot(String username, ChessGame.TeamColor teamColor)  throws DataAccessException{
+
+    }
+    void updateGame(GameModel game)  throws DataAccessException{
+
+    }
+    void removeGame(GameModel game)  throws DataAccessException{
+
+    }
+    void clearGames()  throws DataAccessException{
+
+    }
+}
