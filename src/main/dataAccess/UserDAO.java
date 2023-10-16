a16e5a9998d6b795df42df7782b26619a9bf6ea4
Kyler Moulton <kylermoulton@gmail.com>
2023-10-16 12:48:17 -0600

    User Model and Data Access

---
+++ src/main/dataAccess/UserDAO.java
@@ -0,0 +1,29 @@
+package dataAccess;
+
+import model.UserModel;
+
+import javax.xml.crypto.Data;
+
+public class UserDAO {
+    void CreateUser(UserModel user) throws DataAccessException{
+
+    }
+    UserModel GetUser(UserModel user) throws DataAccessException{
+        return null;
+    }
+    void UpdateUserUsername(UserModel user,String username) throws DataAccessException{
+
+    }
+    void UpdateUserPassword(UserModel user,String password) throws DataAccessException{
+
+    }
+    void UpdateUserEmail(UserModel user, String email) throws DataAccessException{
+
+    }
+    void DeleteUser(UserModel user) throws DataAccessException{
+
+    }
+    void clearUsers() throws DataAccessException{
+
+    }
+}
