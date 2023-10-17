e0f03da8f4cf2a8d05d2ea8d12d9ea0d1cc5f515
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 14:51:44 -0600

    RegisterResult java doc

--- src/main/result/RegisterResult.java
+++ src/main/result/RegisterResult.java
@@ -1,10 +1,19 @@
 package result;
 
+/**
+ * Class that creates a Register Result that LoginService returns
+ */
 public class RegisterResult {
     private String message;
     private String authToken;
     private String username;
 
+    /**
+     * Constructor for RegisterResult
+     * @param authToken Takes in an authToken to check if the User is authorized
+     * @param message Message returned if HTTP request fails or is successful
+     * @param username Takes in the username of the User who logged in
+     */
     public RegisterResult(String message, String authToken, String username) {
         this.message = message;
         this.authToken = authToken;
