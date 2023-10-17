3bdca8ce444fb23116d16aeae49eef5464e6150a
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:31:36 -0600

    LogoutService java doc

--- src/main/service/LogoutService.java
+++ src/main/service/LogoutService.java
@@ -2,8 +2,16 @@ package service;
 
 import result.LogoutResult;
 
+/**
+ * Logs out the user represented by the authToken.
+ */
 public class LogoutService {
-    public LogoutResult logout() {
+    /**
+     * Method that logs out a logged-in User
+     * @return returns a LogoutResult
+     * @throws Exception Throws an exception
+     */
+    public LogoutResult logout() throws Exception{
         return null;
     }
 }
