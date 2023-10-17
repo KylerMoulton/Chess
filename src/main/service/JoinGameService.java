8dd003dd24c3b6f3615b5d2fd1ecd100f17c4e32
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:31:03 -0600

    JoinGameService java doc

--- src/main/service/JoinGameService.java
+++ src/main/service/JoinGameService.java
@@ -3,8 +3,20 @@ package service;
 import request.JoinGameRequest;
 import result.JoinGameResult;
 
+/**
+ * Verifies that the specified game exists, and, if a color is specified,
+ * adds the caller as the requested color to the game.
+ * If no color is specified the user is joined as an observer.
+ * This request is idempotent.
+ */
 public class JoinGameService {
-    public JoinGameResult joinGame(JoinGameRequest j) {
+    /**
+     * Method that joins a game
+     * @param j Takes in a JoinGameRequest
+     * @return Returns a JoinGameResult
+     * @throws Exception Throws an exception
+     */
+    public JoinGameResult joinGame(JoinGameRequest j) throws Exception{
         return null;
     }
 }
