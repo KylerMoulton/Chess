1448a9d1862214bbb02edb9e228807dc8296d62f
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:30:34 -0600

    ClearService java doc

--- src/main/service/ClearService.java
+++ src/main/service/ClearService.java
@@ -2,7 +2,15 @@ package service;
 
 import result.ClearResult;
 
+/**
+ * Clears the database. Removes all users, games, and authTokens
+ */
 public class ClearService {
+    /**
+     * Method that clears the database
+     * @return Returns a ClearResult
+     * @throws Exception Throws an exception
+     */
     public ClearResult clear() throws Exception{
         return null;
     }
