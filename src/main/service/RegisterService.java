ff9b05149d15a19f503c3220177c7b1b4fa670bf
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:31:50 -0600

    RegisterService java doc

--- src/main/service/RegisterService.java
+++ src/main/service/RegisterService.java
@@ -3,7 +3,16 @@ package service;
 import request.RegisterRequest;
 import result.RegisterResult;
 
+/**
+ * Register a new user.
+ */
 public class RegisterService {
+    /**
+     * Method that registers a new user
+     * @param r Takes in a RegisterRequest
+     * @return Returns a RegisterResult
+     * @throws Exception Throws an exception
+     */
     public RegisterResult register(RegisterRequest r) throws Exception{
         return null;
     }
