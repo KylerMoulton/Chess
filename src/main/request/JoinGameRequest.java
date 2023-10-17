352ae3ee008abf8afaacf56b6b436ddb428c16ba
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 12:06:27 -0600

    JoinGameRequest java doc

--- src/main/request/JoinGameRequest.java
+++ src/main/request/JoinGameRequest.java
@@ -1,9 +1,20 @@
 package request;
 
+/**
+ * Class that creates a Join Game request that CreateGameService calls
+ */
+
 public class JoinGameRequest {
     private String authToken;
     private String playerColor;
     private int gameID;
+
+    /**
+     * Constructor for CreateGameRequest
+     * @param authToken Takes in an authToken to verify if the User is authorized
+     * @param playerColor Takes in the color of what team the User wants to join
+     * @param gameID Takes in the gameID of the game to join
+     */
 
     public JoinGameRequest(String authToken, String playerColor, int gameID) {
         this.authToken = authToken;
