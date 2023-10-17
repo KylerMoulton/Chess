b1ad67c189bc3c5227228ff7f581032d8f045d4f
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:31:14 -0600

    ListGamesService java doc

--- src/main/service/ListGamesService.java
+++ src/main/service/ListGamesService.java
@@ -2,8 +2,16 @@ package service;
 
 import result.ListGamesResult;
 
+/**
+ * Gives a list of all games.
+ */
 public class ListGamesService {
-    public ListGamesResult listGames() {
+    /**
+     * Method that lists all games in the Database
+     * @return Returns a ListGamesResult
+     * @throws Exception Throws an exception
+     */
+    public ListGamesResult listGames() throws Exception{
         return null;
     }
 }
