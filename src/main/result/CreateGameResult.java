3afb95cf8b6b227adf1b810e496889b0611cc041
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 12:36:07 -0600

    CreateGameResult java doc

--- src/main/result/CreateGameResult.java
+++ src/main/result/CreateGameResult.java
@@ -1,10 +1,20 @@
 package result;
 
+/**
+ * Class that creates a Create Game Result that CreateGameResult returns
+ */
+
 public class CreateGameResult {
     private String authToken;
     private int gameID;
     private String message;
 
+    /**
+     * Constructor for CreateGameResult
+     * @param authToken Takes in an authToken to check if the User is authorized
+     * @param gameID Takes in the gameID of the created game
+     * @param message Message returned if HTTP request fails or is successful
+     */
     public CreateGameResult(String authToken, int gameID, String message) {
         this.authToken = authToken;
         this.gameID = gameID;
