10fc9cbc4a55491f84e62fbf513e21d7e34fba00
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:30:51 -0600

    CreateGameService java doc

--- src/main/service/CreateGameService.java
+++ src/main/service/CreateGameService.java
@@ -3,8 +3,17 @@ package service;
 import request.CreateGameRequest;
 import result.CreateGameResult;
 
+/**
+ * Creates a new game
+ */
 public class CreateGameService {
-    public CreateGameResult createGame(CreateGameRequest g){
+    /**
+     * Method that create a new game
+     * @param g Takes in a CreateGameRequest
+     * @return Returns a CreateGameResult
+     * @throws Exception Throws an exception
+     */
+    public CreateGameResult createGame(CreateGameRequest g) throws Exception{
         return null;
     }
 }
