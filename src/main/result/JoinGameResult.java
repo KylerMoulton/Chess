8c04e1427f441c8069f941ca26b15ad90952575a
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 12:41:44 -0600

    JoinGameResult java doc

--- src/main/result/JoinGameResult.java
+++ src/main/result/JoinGameResult.java
@@ -1,9 +1,18 @@
 package result;
 
+/**
+ * Class that creates a Join Game Result that JoinGameService returns
+ */
+
 public class JoinGameResult {
     private String authToken;
     private String message;
 
+    /**
+     * Constructor for JoinGameResult
+     * @param authToken Takes in an authToken to check if the User is authorized
+     * @param message Message returned if HTTP request fails or is successful
+     */
     public JoinGameResult(String authToken, String message) {
         this.authToken = authToken;
         this.message = message;
