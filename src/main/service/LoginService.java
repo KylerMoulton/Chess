d8ebde9cefd6447ff52cf34ab538a5b178bdf28f
Kyler Moulton <kylermoulton@gmail.com>
2023-10-17 15:31:24 -0600

    LoginService java doc

--- src/main/service/LoginService.java
+++ src/main/service/LoginService.java
@@ -3,7 +3,16 @@ package service;
 import request.LoginRequest;
 import result.LoginResult;
 
+/**
+ * Logs in an existing user (returns a new authToken).
+ */
 public class LoginService {
+    /**
+     * Method for logging in a user
+     * @param r Takes a LoginRequest
+     * @return Returns a LoginResult
+     * @throws Exception Throws an exception
+     */
     public LoginResult login (LoginRequest r) throws Exception{
         return null;
     }
