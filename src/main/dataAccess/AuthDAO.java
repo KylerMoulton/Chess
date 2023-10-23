6f72d0b0b591a5c214aaaae9a08acbcf8a33d4ff
Kyler Moulton <kylermoulton@gmail.com>
2023-10-23 17:01:09 -0600

    Updated Javadoc

--- src/main/dataAccess/AuthDAO.java
+++ src/main/dataAccess/AuthDAO.java
@@ -9,6 +9,7 @@ public class AuthDAO {
     /**
      * Creates an AuthToken that states if the user is authorized
      * @param token takes in an AuthTokenModel
+     * @throws DataAccessException Throws a DataAccessException
      */
     void createToken(AuthTokenModel token) throws DataAccessException{
     }
@@ -17,6 +18,7 @@ public class AuthDAO {
      * Gets an AuthToken
      * @param token takes in a String of the requested token
      * @return Returns the AuthTokenModel of the request token
+     * @throws DataAccessException Throws a DataAccessException
      */
     AuthTokenModel getToken(String token) throws DataAccessException{
         return null;
@@ -25,6 +27,7 @@ public class AuthDAO {
     /**
      * Updates the requested token
      * @param token takes in a String of the requested token
+     * @throws DataAccessException Throws a DataAccessException
      */
     void updateToken(String token) throws DataAccessException{
 
@@ -33,6 +36,7 @@ public class AuthDAO {
     /**
      * Updates the players username
      * @param username Takes in a String of the new username
+     * @throws DataAccessException Throws a DataAccessException
      */
     void updateUsername(String username) throws DataAccessException{
 
@@ -41,6 +45,7 @@ public class AuthDAO {
     /**
      * Deletes the requested AuthToken
      * @param token takes in a String of the requested token
+     * @throws DataAccessException Throws a DataAccessException
      */
     void deleteToken(AuthTokenModel token) throws DataAccessException{
 
