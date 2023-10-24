1398d0be844a4f66a566ec279f0d2370b43ac250
Kyler Moulton <kylermoulton@gmail.com>
2023-10-24 16:23:16 -0600

    Register Functionality

--- src/main/handlers/RegisterHandler.java
+++ src/main/handlers/RegisterHandler.java
@@ -1,6 +1,8 @@
 package handlers;
 
 import com.google.gson.Gson;
+import exeptions.AlreadyTakenException;
+import exeptions.BadReqException;
 import request.RegisterRequest;
 import result.RegisterResult;
 import service.RegisterService;
@@ -8,16 +10,26 @@ import spark.Request;
 import spark.Response;
 
 public class RegisterHandler {
-    private Gson gson;
-    public Object handleRequest(Request req, Response res) throws Exception {
+    private final Gson gson = new Gson();
+    public Object handleRequest(Request req, Response res) {
         RegisterRequest request = gson.fromJson(req.body(), RegisterRequest.class);
         RegisterService service = new RegisterService();
-        RegisterResult result = service.register(request);
-        //set status
-        res.status(200);
-        res.status(400);
-        res.status(403);
-        res.status(500);
+        RegisterResult result = new RegisterResult(null,null,null);
+        try {
+            result = service.register(request);
+            res.status(200);
+        } catch (BadReqException e) {
+            res.status(400);
+            result.setMessage(e.getMessage());
+        } catch (AlreadyTakenException e) {
+            res.status(403);
+            result.setMessage(e.getMessage());
+        } catch (Exception e) {
+            res.status(500);
+            result.setMessage(e.getMessage());
+
+        }
+
         return gson.toJson(result);
     }
 }
--- src/main/service/RegisterService.java
+++ src/main/service/RegisterService.java
@@ -1,8 +1,12 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.UserDAO;
+import exeptions.AlreadyTakenException;
+import exeptions.BadReqException;
 import request.RegisterRequest;
 import result.RegisterResult;
-
 /**
  * Register a new user.
  */
@@ -14,6 +18,22 @@ public class RegisterService {
      * @throws Exception Throws an exception
      */
     public RegisterResult register(RegisterRequest r) throws Exception{
-        return null;
+        validCredentials(r.getUsername(),r.getPassword(),r.getEmail());
+        UserDAO users = new UserDAO();
+        AuthDAO tokens = new AuthDAO();
+        alreadyTaken(r.getUsername(),users);
+        users.CreateUser(r.getUsername(),r.getPassword(),r.getEmail());
+        String authToken = tokens.createToken(r.getUsername());
+        return new RegisterResult(null, authToken,r.getUsername());
+    }
+    private void validCredentials(String username, String password, String email) throws BadReqException {
+        if (username == null && password == null && email ==null) {
+            throw new BadReqException("Error: bad request");
+        }
+    }
+    private void alreadyTaken(String username,UserDAO users) throws AlreadyTakenException, DataAccessException {
+        if (users.GetUser(username)!=null) {
+            throw new AlreadyTakenException("Error: already taken");
+        }
     }
 }
