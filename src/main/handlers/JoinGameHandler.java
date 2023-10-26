ca012bafde5bab8c20da08ec698071b936482734
Kyler Moulton <kylermoulton@gmail.com>
2023-10-26 12:47:47 -0600

    Join Game Functionality

--- src/main/handlers/JoinGameHandler.java
+++ src/main/handlers/JoinGameHandler.java
@@ -1,6 +1,9 @@
 package handlers;
 
 import com.google.gson.Gson;
+import exeptions.AlreadyTakenException;
+import exeptions.BadReqException;
+import exeptions.UnauthorizedException;
 import request.JoinGameRequest;
 import result.JoinGameResult;
 import service.JoinGameService;
@@ -8,17 +11,29 @@ import spark.Request;
 import spark.Response;
 
 public class JoinGameHandler {
-    private Gson gson;
-    public Object handleRequest(Request req, Response res) throws Exception {
+    private Gson gson = new Gson();
+    public Object handleRequest(Request req, Response res)  {
         JoinGameRequest request = gson.fromJson(req.body(), JoinGameRequest.class);
         JoinGameService service = new JoinGameService();
-        JoinGameResult result = service.joinGame(request);
+        JoinGameResult result = new JoinGameResult(null,null);
         //set status
-        res.status(200);
-        res.status(400);
-        res.status(401);
-        res.status(403);
-        res.status(500);
+        try {
+            String token = req.headers("Authorization");
+            result = service.joinGame(request,token);
+            res.status(200);
+        } catch (BadReqException e) {
+            res.status(400);
+            result.setMessage((e.getMessage()));
+        } catch (UnauthorizedException e) {
+            res.status(401);
+            result.setMessage(e.getMessage());
+        } catch (AlreadyTakenException e) {
+            res.status(403);
+            result.setMessage(e.getMessage());
+        } catch (Exception e) {
+            res.status(500);
+            result.setMessage(e.getMessage());
+        }
         return gson.toJson(result);
     }
 }
--- src/main/service/JoinGameService.java
+++ src/main/service/JoinGameService.java
@@ -1,7 +1,17 @@
 package service;
 
+import dataAccess.AuthDAO;
+import dataAccess.DataAccessException;
+import dataAccess.GameDAO;
+import exeptions.AlreadyTakenException;
+import exeptions.BadReqException;
+import exeptions.UnauthorizedException;
+import model.AuthTokenModel;
+import model.GameModel;
 import request.JoinGameRequest;
 import result.JoinGameResult;
+
+import java.util.Objects;
 
 /**
  * Verifies that the specified game exists, and, if a color is specified,
@@ -16,7 +26,54 @@ public class JoinGameService {
      * @return Returns a JoinGameResult
      * @throws Exception Throws an exception
      */
-    public JoinGameResult joinGame(JoinGameRequest j) throws Exception{
+    public JoinGameResult joinGame(JoinGameRequest j, String token) throws Exception{
+        AuthDAO tokens = new AuthDAO();
+        GameDAO games = new GameDAO();
+        checkAuthorization(token,tokens);
+        GameModel curGame = validGameReq(j.getGameID(),games);
+        assert curGame != null;
+        AuthTokenModel curUser = getCurUser(token,tokens);
+        assert curUser != null;
+        setPlayerColor(j.getPlayerColor(),curGame,curUser);
+        return new JoinGameResult(token,null);
+    }
+
+    private AuthTokenModel getCurUser(String token, AuthDAO authTokens) throws DataAccessException {
+        for (AuthTokenModel tokens : authTokens.getCreatedAuthTokens().values()) {
+            if (Objects.equals(tokens.getAuthToken(), token)) {
+                return tokens;
+            }
+        }
         return null;
+    }
+
+    private void setPlayerColor(String playerColor, GameModel curGame,AuthTokenModel curUser) throws AlreadyTakenException {
+        if (curGame.getWhiteUsername() != null && Objects.equals(playerColor, "WHITE")) {
+            throw new AlreadyTakenException("Error: already taken");
+        } else if (Objects.equals(playerColor, "WHITE")) {
+            curGame.setWhiteUsername(curUser.getUsername());
+        }
+        if (curGame.getBlackUsername() != null && Objects.equals(playerColor, "BLACK")) {
+            throw new AlreadyTakenException("Error: already taken");
+        } else if (Objects.equals(playerColor,"BLACK")) {
+            curGame.setBlackUsername(curUser.getUsername());
+        }
+    }
+
+    private GameModel validGameReq(int gameID, GameDAO games) throws DataAccessException, BadReqException {
+        for (GameModel game : games.getAllGames()) {
+            if (game.getGameID() == gameID) {
+                return game;
+            } else throw new BadReqException("Error: bad request");
+        }
+        return null;
+    }
+
+    public void checkAuthorization(String token, AuthDAO tokens) throws DataAccessException, UnauthorizedException {
+        if (token!=null){
+            if (!Objects.equals(tokens.getToken(token), token)) {
+                throw new UnauthorizedException("Error: unauthorized");
+            }
+        }
     }
 }
