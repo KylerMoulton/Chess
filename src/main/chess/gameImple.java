859b08d328031ac8e748882d2d8706bb66f287d8
Kyler Moulton <kylermoulton@gmail.com>
2023-11-02 15:24:57 -0600

    ChessGame Serialization/Deserialization

--- src/main/chess/gameImple.java
+++ src/main/chess/gameImple.java
@@ -1,12 +1,17 @@
 package chess;
 
+
+import com.google.gson.*;
+
+import java.lang.reflect.Type;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
-public class gameImple implements ChessGame{
+public class gameImple implements ChessGame {
     public TeamColor teamTurn;
     public ChessBoard gameBoard;
+
     @Override
     public TeamColor getTeamTurn() {
         return teamTurn;
@@ -27,19 +32,19 @@ public class gameImple implements ChessGame{
             for (ChessMove move : curPiece.pieceMoves(gameBoard, startPosition)) {
 //                System.out.println(move);
                 ChessPiece tempPiece = null;
-                if (gameBoard.getPiece(move.getEndPosition())!=null) {
+                if (gameBoard.getPiece(move.getEndPosition()) != null) {
                     if (gameBoard.getPiece(move.getEndPosition()).getTeamColor() != curPiece.getTeamColor()) {
                         tempPiece = gameBoard.getPiece(move.getEndPosition());
                     }
                 }
-                gameBoard.addPiece(startPosition,null);
-                gameBoard.addPiece(move.getEndPosition(),curPiece);
-                if (!isInCheck(curPiece.getTeamColor())){
+                gameBoard.addPiece(startPosition, null);
+                gameBoard.addPiece(move.getEndPosition(), curPiece);
+                if (!isInCheck(curPiece.getTeamColor())) {
                     movesValid.add(move);
                 }
-                gameBoard.addPiece(startPosition,curPiece);
-                gameBoard.addPiece(move.getEndPosition(),null);
-                if (tempPiece!=null) {
+                gameBoard.addPiece(startPosition, curPiece);
+                gameBoard.addPiece(move.getEndPosition(), null);
+                if (tempPiece != null) {
                     gameBoard.addPiece(move.getEndPosition(), tempPiece);
                 }
             }
@@ -50,22 +55,23 @@ public class gameImple implements ChessGame{
     @Override
     public void makeMove(ChessMove move) throws InvalidMoveException {
         if (validMoves(move.getStartPosition()).contains(move)) {
-             if (getTeamTurn()==gameBoard.getPiece(move.getStartPosition()).getTeamColor()) {
+            if (getTeamTurn() == gameBoard.getPiece(move.getStartPosition()).getTeamColor()) {
                 gameBoard.addPiece(move.getEndPosition(), gameBoard.getPiece(move.getStartPosition()));
                 gameBoard.addPiece(move.getStartPosition(), null);
-                if (move.getPromotionPiece()!=null) {
+                if (move.getPromotionPiece() != null) {
                     ChessPiece newPiece = piecePromotion(move.getPromotionPiece());
-                    gameBoard.addPiece(move.getEndPosition(),newPiece);
+                    gameBoard.addPiece(move.getEndPosition(), newPiece);
                 }
-                setTeamTurn(this.teamTurn == TeamColor.WHITE ? TeamColor.BLACK:TeamColor.WHITE);
-            }else throw new InvalidMoveException("Invalid Move");
-        }else throw new InvalidMoveException("Invalid Move");
+                setTeamTurn(this.teamTurn == TeamColor.WHITE ? TeamColor.BLACK : TeamColor.WHITE);
+            } else throw new InvalidMoveException("Invalid Move");
+        } else throw new InvalidMoveException("Invalid Move");
 
 //        gameBoard.addPiece(move.getEndPosition(), gameBoard.getPiece(move.getStartPosition()));
 //        gameBoard.addPiece(move.getStartPosition(), null);
     }
+
     public ChessPiece piecePromotion(ChessPiece.PieceType pieceType) {
-        return switch(pieceType){
+        return switch (pieceType) {
             case KING, PAWN -> null;
             case QUEEN -> new queenImple(teamTurn);
             case BISHOP -> new bishopImple(teamTurn);
@@ -73,16 +79,17 @@ public class gameImple implements ChessGame{
             case ROOK -> new rookImple(teamTurn);
         };
     }
+
     @Override
-    public boolean isInCheck(TeamColor teamColor){
-        for (int i=0;i<8;i++) {
+    public boolean isInCheck(TeamColor teamColor) {
+        for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 ChessPosition curPos = new positionImple(i, j);
                 ChessPiece curPiece = getBoard().getPiece(curPos);
                 if (curPiece != null) {
                     if (curPiece.getTeamColor() != teamColor) {
                         for (ChessMove move : curPiece.pieceMoves(gameBoard, curPos)) {
-                            if (gameBoard.getPiece(move.getEndPosition())!=null) {
+                            if (gameBoard.getPiece(move.getEndPosition()) != null) {
                                 if (gameBoard.getPiece(move.getEndPosition()).getPieceType() == ChessPiece.PieceType.KING) {
                                     if (gameBoard.getPiece(move.getEndPosition()).getTeamColor() == teamColor) {
                                         return true;
@@ -96,6 +103,7 @@ public class gameImple implements ChessGame{
         }
         return false;
     }
+
     @Override
     public boolean isInCheckmate(TeamColor teamColor) {
         if (isInCheck(teamColor)) {
@@ -144,5 +152,33 @@ public class gameImple implements ChessGame{
     @Override
     public ChessBoard getBoard() {
         return gameBoard;
+    }
+
+    public static Gson serialization() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.registerTypeAdapter(ChessGame.class, new adapterChessGame());
+        return gsonBuilder.create();
+    }
+
+    public static class adapterChessGame implements JsonDeserializer<ChessGame> {
+        public ChessGame deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(ChessBoard.class, new adapterChessBoard());
+            return builder.create().fromJson(jsonElement, gameImple.class);
+        }
+    }
+
+    public static class adapterChessBoard implements JsonDeserializer<ChessBoard> {
+        public ChessBoard deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            GsonBuilder builder = new GsonBuilder();
+            builder.registerTypeAdapter(ChessBoard.class, new adapterPiece());
+            return builder.create().fromJson(jsonElement, boardImple.class);
+        }
+    }
+
+    public static class adapterPiece implements JsonDeserializer<ChessPiece> {
+        public ChessPiece deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+            return new Gson().fromJson(jsonElement, pieceImple.class);
+        }
     }
 }
