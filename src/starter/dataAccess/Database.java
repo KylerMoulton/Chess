f9b927a53f70a502b5445e08c0dbeee5f08b2b4e
Kyler Moulton <kylermoulton@gmail.com>
2023-11-02 11:12:30 -0600

    Database

--- src/starter/dataAccess/Database.java
+++ src/starter/dataAccess/Database.java
@@ -28,7 +28,7 @@ public class Database {
     // FIXME: Change these fields, if necessary, to match your database configuration
     public static final String DB_NAME = "chess";
     private static final String DB_USERNAME = "root";
-    private static final String DB_PASSWORD = "admin";
+    private static final String DB_PASSWORD = "WaterPolo4me!";
 
     private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306";
 
@@ -63,6 +63,12 @@ public class Database {
      */
     synchronized public void returnConnection(Connection connection) {
         connections.add(connection);
+    }
+
+    synchronized public void closeConnection() throws SQLException {
+        for (Connection connection : connections) {
+            connection.close();
+        }
     }
 }
 
