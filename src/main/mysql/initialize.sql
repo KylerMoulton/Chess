52a186cc5631cec038168d8fb8cabf0cbd22a467
Kyler Moulton <kylermoulton@gmail.com>
2023-11-02 11:13:04 -0600

    SQL initialization

---
+++ src/main/mysql/initialize.sql
@@ -0,0 +1,23 @@
+DROP SCHEMA IF EXISTS chess;
+CREATE SCHEMA chess;
+
+USE chess;
+
+CREATE TABLE auth (
+    id VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id);
+);
+CREATE TABLE game (
+    gameID INT NOT NULL,
+    whiteUsername VARCHAR(255),
+    blackUsername VARCHAR(255),
+    gameName VARCHAR(255) NOT NULL,
+    PRIMARY KEY (gameID)
+);
+CREATE TABLE user (
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (username)
+);
