d203d81c000e0f9e7c8e015e522a5cf18a3e0c7f
Kyler Moulton <kylermoulton@gmail.com>
2023-11-02 11:10:03 -0600

    AuthDAO database access

--- src/main/dataAccess/AuthDAO.java
+++ src/main/dataAccess/AuthDAO.java
@@ -1,48 +1,92 @@
 package dataAccess;
 
 import model.AuthTokenModel;
+import model.UserModel;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.UUID;
 
-/**
- * Class that creates, updates, and deletes AuthTokenModels in the Database
- */
 public class AuthDAO {
-    public static HashMap<String,AuthTokenModel> createdAuthTokens = new HashMap<>();
-    public HashMap<String,AuthTokenModel> getCreatedAuthTokens() {
+    private final Connection database;
+
+    public AuthDAO() throws DataAccessException {
+        this.database = new Database().getConnection();
+    }
+
+    public String createToken(String inputUsername) {
+        String returnedToken = UUID.randomUUID().toString();
+        try (var preparedStatement = database.prepareStatement("Insert INTO auth(id,username) values (?,?)")) {
+            preparedStatement.setString(1, returnedToken);
+            preparedStatement.setString(2, inputUsername);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            System.out.printf(message);
+        }
+        return returnedToken;
+    }
+
+    public AuthTokenModel getToken(String InputAuthToken) {
+        AuthTokenModel returnedAuth = new AuthTokenModel(null, null);
+        try (var preparedStatement = database.prepareStatement("SELECT id,username FROM user WHERE id=?")) {
+            preparedStatement.setString(1, InputAuthToken);
+            try (var user = preparedStatement.executeQuery()) {
+                var authToken = user.getString("id");
+                var username = user.getString("username");
+                returnedAuth.setAuthToken(authToken);
+                returnedAuth.setUsername(username);
+            } catch (SQLException e) {
+                String message = e.getMessage();
+                System.out.printf(message);
+            }
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            System.out.printf(message);
+        }
+        return returnedAuth;
+    }
+
+    public HashMap<String, AuthTokenModel> getCreatedAuthTokens() {
+        HashMap<String, AuthTokenModel> createdAuthTokens = new HashMap<>();
+        AuthTokenModel returnedAuth = new AuthTokenModel(null, null);
+        try (var preparedStatement = database.prepareStatement("SELECT id,username FROM user")) {
+            try (var user = preparedStatement.executeQuery()) {
+                var authToken = user.getString("id");
+                var username = user.getString("username");
+                returnedAuth.setAuthToken(authToken);
+                returnedAuth.setUsername(username);
+                createdAuthTokens.put(returnedAuth.getAuthToken(), returnedAuth);
+            } catch (SQLException e) {
+                String message = e.getMessage();
+                System.out.printf(message);
+            }
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            System.out.printf(message);
+        }
         return createdAuthTokens;
-    }
-    /**
-     * Creates an AuthToken that states if the user is authorized
-     * @param username takes in an AuthTokenModel
-     */
-    public String createToken(String username) {
-        AuthTokenModel newAuthToken = new AuthTokenModel(UUID.randomUUID().toString(),username);
-        storeToken(newAuthToken);
-        return newAuthToken.getAuthToken();
+
     }
 
-    /**
-     * Gets an AuthToken
-     * @param authToken takes in a String of username associated with the requested token
-     * @return Returns the AuthToken of the requested token
-     */
-    public AuthTokenModel getToken(String authToken) {
-        return createdAuthTokens.get(authToken);
-    }
-
-    /**
-     * Deletes the requested AuthToken
-     * @param token takes in a String of the requested token
-     */
     public void deleteToken(String token) {
-        createdAuthTokens.remove(token);
+        try (var preparedStatement = database.prepareStatement("DELETE FROM auth WHERE id=?")) {
+            preparedStatement.setString(1, token);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            System.out.printf(message);
+        }
     }
+
     public void clearTokens() {
-        createdAuthTokens.clear();
-    }
-    public void storeToken(AuthTokenModel tokenModel) {
-        createdAuthTokens.put(tokenModel.getAuthToken(),tokenModel);
+        try (var preparedStatement = database.prepareStatement("DELETE FROM auth")) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            String message = e.getMessage();
+            System.out.printf(message);
+        }
     }
 }
+
